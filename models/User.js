d7f9decf090a27d136e664cea95d2092a5e59bd1
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-08-05 01:43:41 +0300

    Define user schema

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  
+});
+module.exports = User = mongoose.model("users", UserSchema);
