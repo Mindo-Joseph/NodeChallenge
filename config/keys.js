f24d818d9ce26e8202782229c31cd8dd642145b3
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-08-05 03:52:18 +0300

    Add routes for registration and login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://joe_mindo:Mannzxc789@cluster0.ounm7.mongodb.net/database?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://joe_mindo:Mannzxc789@cluster0.ounm7.mongodb.net/database?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
